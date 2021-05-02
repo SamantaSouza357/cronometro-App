8f02f13d646a3c9e62df9a3176675224c4e1ed62
SamantaSouza <samantasouza931@gmail.com>
2021-05-02 18:34:45 -0300

    aplicativo cronometro

--- App.js
+++ App.js
@@ -1,76 +1,74 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet,Image, TouchableOpacity } from 'react-native';
 
 class App extends Component {
 
-  constructor(props) {
+  constructor (props){
     super(props)
     this.state = {
-      textoFrase :  '',
-      img: require('./src/biscoito.png'),
-    };
-
-    this.quebraBiscoito = this.quebraBiscoito.bind(this);
-
-    this.frases = [
-      'A vida trará coisas boas se tiveres paciência.',
-      'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
-      'Não compense na ira o que lhe falta na razão.',
-      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
-      'A maior de todas as torres começa no solo.',
-      'Não há que ser forte. Há que ser flexível.',
-      'Gente todo dia arruma os cabelos, por que não o coração?',
-      'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
-      'A juventude não é uma época da vida, é um estado de espírito.',
-      'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
-      'Dê toda a atenção para a formação dos teus filhos, sobretudo por exemplos de tua própria vida.',
-      'Siga os bons e aprenda com eles.',
-      'Não importa o tamanho da montanha, ela não pode tapar o sol.',
-      'O bom-senso vai mais longe do que muito conhecimento.',
-      'Quem quer colher rosas deve suportar os espinhos.',
-      'São os nossos amigos que nos ensinam as mais valiosas lições.',
-      'Uma iniciativa mal-sucedida não significa o final de tudo. Sempre existe uma nova oportunidade.',
-      'Aquele que se importa com o sentimento dos outros, não é um tolo.',
-      'A adversidade é um espelho que reflete o verdadeiro eu.',
-      'Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.',
-      'Uma bela flor é incompleta sem suas folhas.',
-      'Sem o fogo do entusiasmo, não há o calor da vitória.',
-      'Não há melhor negócio que a vida. A gente há obtém a troco de nada.'
-    ];
+      numero: 0,
+      ultimoValor : null,
+      botao: 'Iniciar'
     }
 
-    quebraBiscoito(){
-      let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    this.timer = null;
 
-      this.setState({
-        textoFrase:  ' " ' + this.frases[numeroAleatorio] + ' " ',
-        img: require('./src/biscoitoAberto.png')
-      })
+    this.iniciar = this.iniciar.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  iniciar(){
+    if(this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null
+
+      this.setState({botao: 'Iniciar'})
+    }else{
+      this.timer = setInterval( () => {
+        this.setState({numero : this.state.numero + 0.1})
+      },100);
+
+      this.setState({botao: 'Parar'})
+
     }
-
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     nome: '',
-  //     input :''
-  //   }
-
-  //  this.entrar = this.entrar.bind(this);
-  // }
+      
+  }
 
 
-
+  limpar(){
+    if(this.timer != null){
+      clearInterval(this.timer)
+      this.timer = null
+    }
+    this.setState({
+      ultimoValor: this.state.numero,
+      numero:0,
+      botao: 'Iniciar'
+    })
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Image source={this.state.img} style={styles.img} />
-        <Text style={styles.frase}>{this.state.textoFrase}</Text>
-        <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
+
+        <Image source={require('./src/cronometro.png')} style={styles.img} />
+
+          <Text style={styles.timer}>{this.state.numero.toFixed(1)}</Text>
+
           <View style={styles.btnArea}>
-            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+            <TouchableOpacity style={styles.botao} onPress={this.iniciar}>
+              <Text style={styles.btnTexto}>{this.state.botao}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.botao} onPress={this.limpar}>
+              <Text style={styles.btnTexto}>Limpar</Text>
+            </TouchableOpacity>
           </View>
-        </TouchableOpacity>
+
+          <View style={styles.areaUltima}>
+              <Text style={styles.textoTempo}>
+                 {this.state.ultimoValor > 0 ? 'Ultimo tempo:  ' + this.state.ultimoValor.toFixed(1) : ''}
+                 </Text>
+            </View>
 
       </View>
     );
@@ -82,36 +80,42 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef'
+  },
+  timer:{
+    marginTop: -160,
+    color: '#FFF',
+    fontSize:65,
+    fontWeight: 'bold'
+  },
 
-  },
-  img: {
-    width: 250,
-    height: 250
-  },
-  frase: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  botao: {
-    width: 230,
-    height: 50,
-    borderWidth: 2,
-    borderColor: '#dd7b22',
-    borderRadius: 25
-  },
-  btnArea: {
-    flex: 1,
+  btnArea:{
     flexDirection: 'row',
+    marginTop: 80,
+    height: 40
+  },
+  botao:{
+    flex:1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFF',
+    height:40,
+    margin : 17,
+    borderRadius: 9
   },
-  btnTexto: {
-    fontSize: 18,
+  btnTexto:{
+    fontSize:20,
     fontWeight: 'bold',
-    color: '#dd7b22'
+    color: '#00aeef'
+  },
+  areaUltima:{
+    marginTop:40
+  },
+  textoTempo:{
+    fontSize:25,
+    fontStyle: 'italic',
+    color: '#FFF',
+    fontWeight: 'bold'
   }
 })
 
